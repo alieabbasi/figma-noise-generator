79e0f5fe8099dd17136b88f97d4b9b784135a06c
Ali Abbasi <aliabac7887@gmail.com>
2023-06-22 11:41:57 +0430

    radius generation with random degree

--- code.ts
+++ code.ts
@@ -6,35 +6,57 @@ figma.ui.onmessage = (pluginMessage: {
   height: number;
   size: number;
   scale: number;
+  x: number;
+  y: number;
 }) => {
   if (pluginMessage.type === "create-shapes") {
-    const { width, height, size, scale } = pluginMessage;
+    const { width, height, size, scale, x: rootX, y: rootY } = pluginMessage;
+
     const rowCount = Math.floor(+height / scale);
     const colCount = Math.floor(+width / scale);
     const mainFrame = figma.createFrame();
+
     mainFrame.resize(width, height);
     mainFrame.fills = [{
       color: { r: 1, g: 1, b: 1 },
       type: "SOLID",
       opacity: 0
     }]
-    for (let i = 0; i < rowCount; i++) {
-      const y = i * scale + scale / 2 - size / 2;
-      for (let j = 0; j < colCount; j++) {
-        const x = j * scale + scale / 2 - size / 2;
-        const dot = figma.createEllipse();
-        dot.resize(size, size);
-        dot.fills = [{
-          color: { r: 1, g: 1, b: 1 },
-          type: "SOLID",
-          opacity: 0.2
-        }]
-        mainFrame.appendChild(dot);
-        dot.x = x;
-        dot.y = y;
-      }
+
+    const totalCount = rowCount * colCount;
+    const maxDistance = Math.max(width, height);
+    console.log("totalCount", totalCount)
+    console.log("maxDistance", maxDistance)
+    const rCoef = Math.pow(maxDistance, 1 / totalCount);
+    // const rCoef = maxDistance / totalCount;
+    let radius = rCoef
+
+    for (let i = 0; i < totalCount; i++) {
+      const deg = Math.random() * 2 * Math.PI;
+      const deltaY = radius * Math.cos(deg);
+      const deltaX = radius * Math.sin(deg);
+
+      const dot = figma.createEllipse();
+      dot.resize(size, size);
+      dot.fills = [{
+        color: { r: 1, g: 1, b: 1 },
+        type: "SOLID",
+        opacity: 1
+      }]
+      dot.effects = [{
+        type: "LAYER_BLUR",
+        radius: 0.5,
+        visible: true
+      }]
+      dot.effects
+      mainFrame.appendChild(dot);
+      dot.x = rootX + deltaX;
+      dot.y = rootY + deltaY;
+      radius = radius * rCoef;
     }
+
     //delete next 3 lines
+    
     const parentFrame = figma.root.findOne(node => node.type === "FRAME" && node.name === "Frame 14741") as FrameNode;
     if (parentFrame) parentFrame.appendChild(mainFrame);
     else console.log("Frame not found")
