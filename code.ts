f5fc6abe74d821317304a1c9260de4456cc41849
Ali Abbasi <aliabac7887@gmail.com>
2023-06-22 12:51:50 +0430

    minor optimization [radius-random]

--- code.ts
+++ code.ts
@@ -24,24 +24,28 @@ figma.ui.onmessage = (pluginMessage: {
     }]
 
     const totalCount = rowCount * colCount;
-    const maxDistance = Math.max(width, height);
-    console.log("totalCount", totalCount)
-    console.log("maxDistance", maxDistance)
+    const d1 = Math.sqrt(Math.pow(rootX, 2) + Math.pow(rootY, 2));
+    const d2 = Math.sqrt(Math.pow(width - rootX, 2) + Math.pow(rootY, 2));
+    const d3 = Math.sqrt(Math.pow(rootX, 2) + Math.pow(height - rootY, 2));
+    const d4 = Math.sqrt(Math.pow(width - rootX, 2) + Math.pow(height - rootY, 2));
+    const maxDistance = Math.max(d1, d2, d3, d4);
     const rCoef = Math.pow(maxDistance, 1 / totalCount);
     // const rCoef = maxDistance / totalCount;
-    let radius = rCoef
+    let radius = rCoef;
 
     for (let i = 0; i < totalCount; i++) {
       const deg = Math.random() * 2 * Math.PI;
       const deltaY = radius * Math.cos(deg);
+      const dotY = rootY + deltaY;
       const deltaX = radius * Math.sin(deg);
+      const dotX = rootX + deltaX;
 
       const dot = figma.createEllipse();
       dot.resize(size, size);
       dot.fills = [{
         color: { r: 1, g: 1, b: 1 },
         type: "SOLID",
-        opacity: 1
+        opacity: 0.5
       }]
       dot.effects = [{
         type: "LAYER_BLUR",
@@ -50,13 +54,13 @@ figma.ui.onmessage = (pluginMessage: {
       }]
       dot.effects
       mainFrame.appendChild(dot);
-      dot.x = rootX + deltaX;
-      dot.y = rootY + deltaY;
+      dot.x = dotX;
+      dot.y = dotY;
       radius = radius * rCoef;
     }
 
     //delete next 3 lines
-    
+
     const parentFrame = figma.root.findOne(node => node.type === "FRAME" && node.name === "Frame 14741") as FrameNode;
     if (parentFrame) parentFrame.appendChild(mainFrame);
     else console.log("Frame not found")
