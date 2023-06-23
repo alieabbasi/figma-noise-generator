77eb3d20f13cbcf4c8ae42446c6e2fbcc29811bc
Ali Abbasi <aliabac7887@gmail.com>
2023-06-23 18:46:56 +0330

    semi normal distribution

--- code.ts
+++ code.ts
@@ -8,39 +8,51 @@ figma.ui.onmessage = (pluginMessage: {
   scale: number;
 }) => {
   if (pluginMessage.type === "create-shapes") {
-    const { width, height, size, scale } = pluginMessage;
+    const { width, height, size } = pluginMessage;
+    const num = 5;
+    const scale = pluginMessage.scale * num;
     const rowCount = Math.floor(+height / scale);
     const colCount = Math.floor(+width / scale);
+    console.log("rowCount", rowCount);
+    console.log("colCount", colCount);
+    
     const mainFrame = figma.createFrame();
     mainFrame.resize(width, height);
-    mainFrame.fills = [{
-      color: { r: 1, g: 1, b: 1 },
-      type: "SOLID",
-      opacity: 0
-    }]
+    mainFrame.fills = [
+      {
+        color: { r: 1, g: 1, b: 1 },
+        type: "SOLID",
+        opacity: 0,
+      },
+    ];
     for (let i = 0; i < rowCount; i++) {
-      const y = i * scale + scale / 2 - size / 2;
       for (let j = 0; j < colCount; j++) {
-        const x = j * scale + scale / 2 - size / 2;
-        const dot = figma.createEllipse();
-        dot.resize(size, size);
-        dot.fills = [{
-          color: { r: 1, g: 1, b: 1 },
-          type: "SOLID",
-          opacity: 0.2
-        }]
-        mainFrame.appendChild(dot);
-        dot.x = x;
-        dot.y = y;
+        for (let k = 0; k < Math.pow(num, 2); k++) {
+          const x = Math.random() * scale + j * scale - size / 2;
+          const y = Math.random() * scale + i * scale - size / 2;
+          const dot = figma.createEllipse();
+          dot.resize(size, size);
+          dot.fills = [
+            {
+              color: { r: 0, g: 0, b: 0 },
+              type: "SOLID",
+            },
+          ];
+          mainFrame.appendChild(dot);
+          dot.x = x;
+          dot.y = y;
+        }
       }
     }
     //delete next 3 lines
-    const parentFrame = figma.root.findOne(node => node.type === "FRAME" && node.name === "Frame 14741") as FrameNode;
+    const parentFrame = figma.root.findOne(
+      (node) => node.type === "FRAME" && node.name === "Frame 14741"
+    ) as FrameNode;
     if (parentFrame) parentFrame.appendChild(mainFrame);
-    else console.log("Frame not found")
-    figma.viewport.scrollAndZoomIntoView([mainFrame])
+    else console.log("Frame not found");
 
+    figma.viewport.scrollAndZoomIntoView([mainFrame]);
   }
 
-  figma.closePlugin()
+  figma.closePlugin();
 };
